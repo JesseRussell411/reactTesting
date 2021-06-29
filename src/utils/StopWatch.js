8f00b7ba5e8b0e98edf98725cc534c39e8d0a51d
jesse russell <jesserussell4@montana.edu>
2021-06-29 14:05:01 -0600

    changed tsconfig and updated stopwatch

--- src/utils/StopWatch.js
+++ src/utils/StopWatch.js
@@ -1,55 +1,106 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var StopWatch = /** @class */ (function () {
-    function StopWatch() {
+/**
+ * Use for measuring the passage of time in milliseconds. Very similar to C#'s stopwatch (basically a ripoff).
+ */
+export default class Stopwatch {
+    constructor() {
+        // why is this not called startTime?: because of the paused state. see start()
+        this.earlyTime = null;
+        this.stopTime = null;
+        // this class can be in 3 states:
+        // reset:
+        //   earlyTime === null
+        //   stopTime === anything
+        //
+        // running:
+        //   earlyTime !== null
+        //   stopTime === null
+        //
+        // paused:
+        //   earlyTime !== null
+        //   stopTime !== null
+        /**
+         * Starts the stopwatch.
+         */
+        this.start = () => {
+            if (this.earlyTime === null) {
+                //currently reset
+                this.earlyTime = new Date();
+            }
+            else if (this.stopTime !== null) {
+                // currently paused
+                // Add the time in the paused state to earlyTime so the time in the paused state is not counted later.
+                // This part is why earlyTime is not called startTime, since the startTime would drift forward from the actual startTime.
+                this.earlyTime = new Date(this.earlyTime.getTime() + this.elapsedWhilePausedInMilliseconds);
+            }
+            this.stopTime = null;
+        };
+        /**
+         * Pauses the stopwatch
+         */
+        this.stop = () => {
+            this.stopTime = new Date();
+        };
+        /**
+         * Resets the stopwatch
+         */
+        this.reset = () => {
+            this.earlyTime = null;
+            this.stopTime = null;
+        };
+        /**
+         * Resets, then starts the stopwatch
+         */
+        this.restart = () => {
+            this.earlyTime = new Date();
+            this.stopTime = null;
+        };
+        // this is where get elapsedTime would be if javascript had a timeInterval class. Date could be a substitute, but that doesn't really make sense.
     }
-    StopWatch.prototype.start = function () {
-        if (this.startTime === undefined) {
-            this.startTime = new Date();
+    /**
+     * Whether the stopwatch has been reset, and therefore, no time has elapsed.
+     */
+    get isReset() {
+        return this.earlyTime === null;
+    }
+    /**
+     * Whether the stopwatch is not paused or reset. If it's running.
+     */
+    get isRunning() {
+        return this.stopTime !== null && !this.isReset;
+    }
+    /**
+     * Whether the stopwatch has been paused by the stop method. returns false if the stopwatch has been reset, as long as it hasn't been started yet.
+     */
+    get isPaused() {
+        return this.earlyTime !== null && this.stopTime !== null;
+    }
+    /**
+     * Time since the stopwatch was started or restarted minus any time in which it was paused.
+     */
+    get elapsedTimeInMilliseconds() {
+        if (this.earlyTime === null) {
+            // currently reset
+            return 0;
         }
-        this.stopTime = undefined;
-    };
-    StopWatch.prototype.stop = function () {
-        this.stopTime = new Date();
-    };
-    StopWatch.prototype.reset = function () {
-        this.startTime = undefined;
-        this.stopTime = undefined;
-    };
-    StopWatch.prototype.restart = function () {
-        this.reset();
-        this.start();
-    };
-    Object.defineProperty(StopWatch.prototype, "isReset", {
-        get: function () {
-            return this.startTime === undefined;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(StopWatch.prototype, "isRunning", {
-        get: function () {
-            return this.stopTime !== undefined && !this.isReset;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(StopWatch.prototype, "elapsedTimeInMilliseconds", {
-        get: function () {
-            if (this.startTime === undefined) {
-                return 0;
-            }
-            else if (this.stopTime === undefined) {
-                return new Date().getTime() - this.startTime.getTime();
-            }
-            else {
-                return this.stopTime.getTime() - this.startTime.getTime();
-            }
-        },
-        enumerable: false,
-        configurable: true
-    });
-    return StopWatch;
-}());
-exports.default = StopWatch;
+        else if (this.stopTime === null) {
+            //currently running
+            return new Date().getTime() - this.earlyTime.getTime();
+        }
+        else {
+            // currently paused
+            return this.stopTime.getTime() - this.earlyTime.getTime();
+        }
+    }
+    /**
+     * If the stopwatch is paused, returns how long it has been paused.
+     */
+    get elapsedWhilePausedInMilliseconds() {
+        if (this.earlyTime !== null && this.stopTime !== null) {
+            return new Date().getTime() - this.stopTime.getTime();
+        }
+        else {
+            return 0;
+        }
+    }
+}
 ;
--- src/utils/StopWatch.ts
+++ src/utils/StopWatch.ts
@@ -1,40 +1,41 @@
 /**
- * Use for measuring passage of time in milliseconds. Very similar to C#'s stopwatch (basically a ripoff).
+ * Use for measuring the passage of time in milliseconds. Very similar to C#'s stopwatch (basically a ripoff).
  */
  export default class Stopwatch {
-    private earlyTime?: Date = undefined;
-    private stopTime?: Date = undefined;
+    // why is this not called startTime?: because of the paused state. see start()
+    private earlyTime: Date = null;
+    private stopTime: Date = null;
 
     // this class can be in 3 states:
     // reset:
-    //   earlyTime === undefined
+    //   earlyTime === null
     //   stopTime === anything
     //
     // running:
-    //   earlyTime !== undefined
-    //   stopTime === undefined
+    //   earlyTime !== null
+    //   stopTime === null
     //
     // paused:
-    //   earlyTime !== undefined
-    //   stopTime !== undefined
+    //   earlyTime !== null
+    //   stopTime !== null
 
     /**
      * Starts the stopwatch.
      */
     readonly start = (): void => {
-        if (this.earlyTime === undefined) {
+        if (this.earlyTime === null) {
             //currently reset
             this.earlyTime = new Date();
-        } else if (this.stopTime !== undefined) {
+        } else if (this.stopTime !== null) {
             // currently paused
 
             // Add the time in the paused state to earlyTime so the time in the paused state is not counted later.
-            // This part is why earlyTime is not called startTime
+            // This part is why earlyTime is not called startTime, since the startTime would drift forward from the actual startTime.
             this.earlyTime = new Date(
                 this.earlyTime.getTime() + this.elapsedWhilePausedInMilliseconds
             );
         }
-        this.stopTime = undefined;
+        this.stopTime = null;
     }
 
     /**
@@ -48,8 +49,8 @@
      * Resets the stopwatch
      */
     readonly reset = (): void => {
-        this.earlyTime = undefined;
-        this.stopTime = undefined;
+        this.earlyTime = null;
+        this.stopTime = null;
     }
 
     /**
@@ -57,38 +58,38 @@
      */
     readonly restart = (): void => {
         this.earlyTime = new Date();
-        this.stopTime = undefined;
+        this.stopTime = null;
     }
 
     /**
      * Whether the stopwatch has been reset, and therefore, no time has elapsed.
      */
     get isReset(): boolean {
-        return this.earlyTime === undefined;
+        return this.earlyTime === null;
     }
 
     /**
      * Whether the stopwatch is not paused or reset. If it's running.
      */
     get isRunning(): boolean {
-        return this.stopTime !== undefined && !this.isReset;
+        return this.stopTime !== null && !this.isReset;
     }
 
     /**
      * Whether the stopwatch has been paused by the stop method. returns false if the stopwatch has been reset, as long as it hasn't been started yet.
      */
     get isPaused(): boolean {
-        return this.earlyTime !== undefined && this.stopTime !== undefined;
+        return this.earlyTime !== null && this.stopTime !== null;
     }
 
     /**
      * Time since the stopwatch was started or restarted minus any time in which it was paused.
      */
     get elapsedTimeInMilliseconds(): number {
-        if (this.earlyTime === undefined) {
+        if (this.earlyTime === null) {
             // currently reset
             return 0;
-        } else if (this.stopTime === undefined) {
+        } else if (this.stopTime === null) {
             //currently running
             return new Date().getTime() - this.earlyTime.getTime();
         } else {
@@ -101,7 +102,7 @@
      * If the stopwatch is paused, returns how long it has been paused.
      */
     get elapsedWhilePausedInMilliseconds(): number {
-        if (this.earlyTime !== undefined && this.stopTime !== undefined) {
+        if (this.earlyTime !== null && this.stopTime !== null) {
             return new Date().getTime() - this.stopTime.getTime();
         } else {
             return 0;
