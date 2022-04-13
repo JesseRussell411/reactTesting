4f81746d3b9a52ecb228fe0fd581ed482ef97205
Jesse Russell <jesserussell411@gmail.com>
2022-04-12 22:17:55 -0600

    updated timing

--- src/timing.ts
+++ src/timing.ts
@@ -1,86 +1,96 @@
-import {useEffect, useRef} from "react";
-
+import { useEffect, useRef } from "react";
 
 /**
- * @callback HookTimeout
- * @param handler
- * @param timeout
- * @param args
+ * React safe version of setTimeout.
  * @returns The id of the created timeout or undefined if the component has unmounted.
  */
+export type HookSetTimeout = (
+    handler: () => void,
+    timeout?: number
+) => number | undefined;
 
 /**
  * Creates a react-safe version of setTimeout. All timeouts created will be cleared if the component unmounts.
- * @returns {HookTimeout}
+ * Once that component has unmounted, no new timeouts can be made.
  */
-export function useTimeout() {
-    const {current: ids} = useRef(new Set<number>());
-    const closedRef = useRef(false);
+export function useTimeout(): HookSetTimeout {
+    const { current: ids } = useRef(new Set<number>());
+    const isUnMountedRef = useRef(false);
 
-    useEffect(() => () => {
-        closedRef.current = true;
+    useEffect(
+        () => () => {
+            isUnMountedRef.current = true;
 
-        for (const id of ids) {
-            clearTimeout(id);
-        }
-    }, [ids]);
+            for (const id of ids) {
+                clearTimeout(id);
+            }
+        },
+        [ids]
+    );
 
-    return (handler: (...args:any) => void, timeout?:number, ...args:any[]) => {
-        if (closedRef.current) return undefined;
+    return (handler, timeout) => {
+        // Don't set the timeout if the component has unmounted.
+        if (isUnMountedRef.current) return undefined;
 
-        const modifiedHandler = (...args:any[]) => {
-            clearTimeout(id);
-            handler(...args);
-        }
+        // Add ids.delete to the handler so that it's id is removed from the set once it runs
+        // since a timeout handler can't run more than once like an interval handler.
+        const selfClearHandler = () => {
+            ids.delete(id);
+            handler();
+        };
 
-        const id = window.setTimeout(modifiedHandler, timeout, ...args);
-
+        const id = window.setTimeout(selfClearHandler, timeout);
         ids.add(id);
 
         return id;
-    }
+    };
 }
 
 /**
- * @callback HookInterval
- * @param handler
- * @param timeout
- * @param args
+ * React safe version of setInterval.
  * @returns The id of the created interval or undefined if the component has unmounted.
  */
+export type HookSetInterval = (
+    handler: () => void,
+    interval?: number
+) => number | undefined;
 
 /**
- * Creates a react-safe version of setInterval. All intervals created will be cleared if the component unmounts.
- * @returns {HookInterval}
+ * Creates a react-safe version of setTimeout. All timeouts created will be cleared if the component unmounts.
+ * Once that component has unmounted, no new timeouts can be made.
  */
+export function useInterval(): HookSetInterval {
+    const { current: ids } = useRef<number[]>([]);
+    const isUnMountedRef = useRef(false);
 
-export function useInterval() {
-    const {current: ids} = useRef<number[]>([]);
-    const closedRef = useRef(false);
+    useEffect(
+        () => () => {
+            isUnMountedRef.current = true;
 
-    useEffect(() => () => {
-        closedRef.current = true;
+            for (const id of ids) {
+                clearTimeout(id);
+            }
+        },
+        [ids]
+    );
 
-        for (const id of ids) {
-            clearTimeout(id);
-        }
-    }, [ids]);
+    return (handler, interval) => {
+        // Don't set the interval if the component has unmounted.
+        if (isUnMountedRef.current) return undefined;
 
-    return (handler: (...args:any) => void, timeout?:number, ...args:any[]) => {
-        if (closedRef.current) return undefined;
-
-        const id = window.setInterval(handler, timeout, ...args);
-
+        const id = window.setInterval(handler, interval);
         ids.push(id);
 
         return id;
-    }
+    };
 }
 
-export function useLifespanInterval(handler: () => void, timeout?:number){
+/**
+ * Runs the given handler function at the given interval for the lifespan of the component.
+ */
+export function useLifespanInterval(handler: () => void, interval?: number) {
     useEffect(() => {
-        const id = setInterval(handler, timeout);
+        const id = setInterval(handler, interval);
         return () => clearInterval(id);
-    })
+    });
 }
-
